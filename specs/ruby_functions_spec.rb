1de37e4160f15d30f8495abd61a355c51a29f358
Rob Wilson <rob.wilson195@gmail.com>
2018-10-16 15:41:54 +0100

    Solved second

--- specs/ruby_functions_spec.rb
+++ specs/ruby_functions_spec.rb
@@ -4,16 +4,16 @@ require_relative( '../ruby_functions_practice' )
 
 class FunctionsTest < MiniTest::Test
 
-  def test_return_10()
-    return_10_result = return_10()
-    assert_equal( 10, return_10_result )
-  end
-  #
-  # def test_add()
-  #   add_result = add( 1, 2 )
-  #   assert_equal( 3, add_result )
+  # def test_return_10()
+  #   return_10_result = return_10()
+  #   assert_equal( 10, return_10_result )
   # end
-  #
+
+  def test_add()
+    add_result = add( 1, 2 )
+    assert_equal( 3, add_result )
+  end
+
   # def test_subtract()
   #   subtract_result = subtract( 10, 5 )
   #   assert_equal( 5, subtract_result )
