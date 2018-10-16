769a50cfeece10b13b8f5dd7a1919a0faae226aa
Rob Wilson <rob.wilson195@gmail.com>
2018-10-16 15:51:07 +0100

    a few more functions made

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -5,3 +5,19 @@ end
 def add(num1, num2)
   return num1 + num2
 end
+
+def subtract(num1, num2)
+  return num1 - num2
+end
+
+def multiply(num1, num2)
+  return num1 * num2
+end
+
+def divide(num1, num2)
+  return num1 / num2
+end
+
+def length_of_string(word)
+  return word.length
+end
